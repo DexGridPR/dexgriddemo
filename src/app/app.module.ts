3844b105216531b62ee51d1d7a8656a08ec5bbf5
vincenz5 <vincenzbuhler@gmail.com>
2019-10-21 17:21:26 -0500

    Added initial content, top bar, favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
