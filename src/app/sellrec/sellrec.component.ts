3d47a69896aacf4939fbf0cc0cdb43e72141c095
vincenz5 <vincenzbuhler@gmail.com>
2019-11-04 18:17:34 -0500

    Added JS to lower REC number on sellrec

--- src/app/sellrec/sellrec.component.ts
+++ src/app/sellrec/sellrec.component.ts
@@ -10,14 +10,50 @@ export class SellrecComponent implements OnInit {
 
   constructor(private _web3: Web3Service) { }
 
+  n: number = 13;
+
   ngOnInit() {
-    this._web3.Metamask();
+    // var timesclicked;
+    // timesclicked = 0;
+    var n = this.n;
+    document.getElementById("creditnumber").innerHTML = n;
+
+  }
+
+  changec() {
+    // if (timesclicked = 0) {
+    //   var timesclicked = this.timesclicked;
+    // }
+
+    // else {
+    //   timesclicked++;
+    // }
+
+    var timesclicked;
+    var n = this.n;
+    if (timesclicked > 0 ) {
+      timesclicked = timesclicked + 1;
+      n = n - 1;
+    }
+    else {
+      timesclicked = 0;
+      timesclicked = timesclicked + 1;
+      n = n - 1;
+    }
+
+
+
+    console.log("timesclicked:", timesclicked);
+
+    console.log("New n:", n);
+
+    document.getElementById("creditnumber").innerHTML = n;
   }
 
   transfer() {
     console.log("Transfer RECs");
     // alert("Sending RECs");
-    this._web3.Transfer();
+    // this._web3.Transfer();
   }
 
   
