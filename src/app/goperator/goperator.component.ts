8f0df443ccd942e5a2fbb05836cb4fe6e8fccdfe
vincenz5 <vincenzbuhler@gmail.com>
2019-11-12 12:51:42 -0500

    Made changes to goperator and regacc

--- src/app/goperator/goperator.component.ts
+++ src/app/goperator/goperator.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoperatorComponent implements OnInit {
 
-  public regulatoraccounts = true;
+  public regulatoraccounts = false;
 
   constructor() { }
 
